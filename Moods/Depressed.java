d558630c0af1d19b8c5860336826f23814cb7150
Alex-Huleatt <Ahuleatt@gmail.com>
2015-10-09 23:52:01 -0600

    Moods and helper functions

---
+++ Moods/Depressed.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Oliver.Moods;
+
+import battlecode.common.RobotController;
+
+/**
+ *
+ * @author alexhuleatt
+ */
+public class Depressed implements Mood {
+
+    public void act(RobotController rc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Mood transition(RobotController rc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
