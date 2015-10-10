eeb555c76017eb7ee22f7a9c30467c8da32031db
Alex-Huleatt <Ahuleatt@gmail.com>
2015-10-10 00:31:09 -0600

    try again

--- Const.java
+++ Const.java
@@ -2,6 +2,7 @@ package Oliver;
 
 import battlecode.common.GameObject;
 import battlecode.common.MapLocation;
+import battlecode.common.Robot;
 import battlecode.common.RobotController;
 import battlecode.common.RobotInfo;
 
@@ -20,5 +21,16 @@ public class Const {
             }
         }
         return obs[mindex];
+    }
+    
+    public static Robot[] robotFilter(GameObject[] obs) {
+        Robot[] r = new Robot[obs.length];
+        int r_count = 0;
+        for (GameObject ob : obs) {
+            if (ob instanceof Robot) r[r_count++] = (Robot) ob;
+        }
+        Robot[] ret = new Robot[r_count];
+        System.arraycopy(r, 0, ret, 0, ret.length);
+        return ret;
     }
 }
