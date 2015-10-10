93d4169faaab6e708e2078351d1f4fb665642ed1
mackenzie-o <mackenzie.obleness@gmail.com>
2015-10-10 00:28:45 -0600

    bug v0.1

--- Const.java
+++ Const.java
@@ -8,5 +8,30 @@ import battlecode.common.RobotType;
 /* Should probably comment or something */
 
 public class Const {
-    // TODO: direction constants
+    static int directionToInt(Direction d) {
+        switch (d) {
+            case NORTH:
+                return 0;
+            case NORTH_EAST:
+                return 1;
+            case EAST:
+                return 2;
+            case SOUTH_EAST:
+                return 3;
+            case SOUTH:
+                return 4;
+            case SOUTH_WEST:
+                return 5;
+            case WEST:
+                return 6;
+            case NORTH_WEST:
+                return 7;
+            default:
+                return -1;
+        }
+    }
+
+    static Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST,
+            Direction.EAST, Direction.SOUTH_EAST, Direction.SOUTH,
+            Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
 }
--- Soldier.java
+++ Soldier.java
@@ -1,44 +1,84 @@
 package Oliver;
 
-import battlecode.common.Direction;
-import battlecode.common.GameConstants;
-import battlecode.common.RobotController;
-import battlecode.common.RobotType;
+import battlecode.common.*;
+import java.util.Random;
 
 /* Should probably comment or something */
 
 public class Soldier {
     RobotController rc;
+    int goalSlope;
+    MapLocation enemyHQ;
+    Random rand = new Random();
     
     public Soldier(RobotController bot) {
         rc = bot;
     }
 
     public void run() throws Exception {
-        if (rc.isActive()) {
-            if (Math.random() < 0.005) {
-                // Lay a mine
-                if (rc.senseMine(rc.getLocation()) == null)
-                    rc.layMine();
-            } else {
-                // Choose a random direction, and move that way if possible
-                Direction dir = Direction.values()[(int) (Math.random() * 8)];
-                if (rc.canMove(dir)) {
-                    rc.move(dir);
-                    rc.setIndicatorString(0, "Last direction moved: " + dir.toString());
+        enemyHQ = rc.senseEnemyHQLocation();
+        while (true) {
+            if (rc.isActive()) {
+                if (Math.random() < 0.005) {
+                    // Lay a mine
+                    if (rc.senseMine(rc.getLocation()) == null)
+                        rc.layMine();
+                } else {
+                    moveTowards(enemyHQ);
                 }
             }
-        }
 
-        if (Math.random() < 0.01 && rc.getTeamPower() > 5) {
-            // Write the number 5 to a position on the message board corresponding to the robot's ID
-            rc.broadcast(rc.getRobot().getID() % GameConstants.BROADCAST_MAX_CHANNELS, 5);
+            if (Math.random() < 0.01 && rc.getTeamPower() > 5) {
+                // Write the number 5 to a position on the message board corresponding to the robot's ID
+                rc.broadcast(rc.getRobot().getID() % GameConstants.BROADCAST_MAX_CHANNELS, 5);
+            }
+            rc.yield();
         }
-        rc.yield();
     }
     
-    public void nextMove() {
-        // TODO: bug bug buggin
+    public void move(Direction dir) throws Exception{
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        } else {
+            System.out.println("rc failed to move in dir");
+        }
+    }
+    
+    public void moveTowards(MapLocation goal) throws Exception {
+        MapLocation myLoc = rc.getLocation();
+        if(!isObstacle(myLoc.directionTo(enemyHQ))) {
+            move(myLoc.directionTo(enemyHQ));
+            return;
+        }
+        
+        // bugggg
+        int obsOne, obsTwo;
+        for (int i = 0; i < 8; i++) {
+            if (isObstacle(Const.directions[i])) {
+                continue;
+            }
+            obsOne = (i + 1) % 8;
+            obsTwo = (i + 2) % 8;
+            if (isObstacle(Const.directions[obsOne]) && (i%2 == 1 || isObstacle(Const.directions[obsTwo]))){
+                move(Const.directions[i]);
+                return;
+            }
+        }
+        System.out.println("Could not bug :(");
+    }
+    
+    public boolean isObstacle(MapLocation loc) {
+        TerrainTile tt = rc.senseTerrainTile(loc);
+        if (tt == TerrainTile.VOID) return true;
+        if (rc.canSenseSquare(loc)) {
+            
+            if (ri != null && (rand.nextDouble() > .8 || ri.type == RobotType.HQ)) return true; //
+        }
+        return false;
+    }
+    
+    public boolean isObstacle(Direction dir) {
+        return isObstacle(rc.getLocation().add(dir));
     }
 }
 
