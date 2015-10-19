41c6ef4a2062a39a5071a901cedbe42050642363
Alex-Huleatt <Ahuleatt@gmail.com>
2015-10-18 21:28:36 -0600

    yay

---
+++ Moods/HQ/Threatened.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Oliver.Moods.HQ;
+
+import Oliver.Const;
+import Oliver.Units.HQ;
+import Oliver.Moods.Mood;
+import Oliver.Units.Unit;
+import battlecode.common.Direction;
+import battlecode.common.GameObject;
+import battlecode.common.Robot;
+import battlecode.common.RobotType;
+
+/**
+ *
+ * @author alexhuleatt
+ */
+public class Threatened extends Mood {
+
+    public Threatened(Unit u) {
+        super(u);
+    }
+
+    @Override
+    public Mood swing() throws Exception {
+        GameObject[] things = rc.senseNearbyGameObjects(Robot.class, RobotType.HQ.sensorRadiusSquared, team.opponent());
+        if (things.length > 0) {
+            Robot[] ri_arr = Const.robotFilter(things, team.opponent());
+            if (ri_arr.length == 0) {
+                rc.broadcast(0, 0);
+                return new Happy((HQ) u);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void act() throws Exception {
+//        if (rc.getLocation().distanceSquaredTo(rc.senseEnemyHQLocation()) > 300 && rc.isActive() && !rc.hasUpgrade(Upgrade.DEFUSION)) {
+//            rc.researchUpgrade(Upgrade.DEFUSION);
+//        }
+
+        if (rc.isActive()) {
+            // Spawn a soldier
+            Direction dir = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
+            if (rc.canMove(dir)) {
+                rc.spawn(dir);
+            }
+
+        }
+
+        if (rc.isActive()) {
+            simpleAttack();
+        }
+        rc.yield();
+
+    }
+    
+    public String toString() {return ":O";}
+
+}
---
+++ Scheme/Strat.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Oliver.Scheme;
+
+import battlecode.common.RobotController;
+
+/**
+ * This should be the core of everything. 
+ * HQ will have this instance, this should broadcast major changes 
+ * in macro strats as well as important info like which types of encampments we
+ * want.
+ * @author alexhuleatt
+ */
+public class Strat {
+    
+    RobotController rc; //hq
+    
+    public Strat(RobotController rc) {
+        this.rc = rc;
+    }
+    
+    
+}
