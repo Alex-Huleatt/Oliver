c361ac9960bb5bb4a9a5f69124dd25c01212a944
Alex-Huleatt <Ahuleatt@gmail.com>
2015-10-19 18:49:22 -0600

    STRATEGY

---
+++ Moods/DefaultMood.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team016.Moods;
+
+import team016.Units.Unit;
+
+/**
+ *
+ * @author alexhuleatt
+ */
+public class DefaultMood extends Mood {
+
+    public DefaultMood(Unit u) {
+        super(u);
+    }
+    
+}
