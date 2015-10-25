e21f684e10d7cc859fb9c7607f2624d7009808ca
Alex-Huleatt <Ahuleatt@gmail.com>
2015-10-25 16:01:40 -0600

    HAI

---
+++ Units/Supplier.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team016.Units;
+
+import battlecode.common.RobotController;
+
+/**
+ *
+ * @author alexhuleatt
+ */
+public class Supplier implements Unit  {
+
+    public Supplier(RobotController rc) {
+        //super(rc);
+    }
+
+    public RobotController getRC() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
